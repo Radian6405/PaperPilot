3180a53c52c6da24999cde609ac63ab79253e091
Sai Vardhan <vardhan.6405@gmail.com>
2024-02-25 19:21:07 +0530

    styled register page

--- paper_pilot/frontend/src/components/pages/Register.js
+++ paper_pilot/frontend/src/components/pages/Register.js
@@ -1,17 +1,49 @@
 import React, { Component } from "react";
+import logo from "../../../static/images/logo 512.png";
 import CSRFtoken from "../helpers/CSRFtoken";
+import {
+  TextField,
+  PasswordField,
+  SubmitField,
+  HelperLabel,
+} from "../helpers/LoginComponents";
 
 export default function Register() {
   return (
     <>
-      <form action="/api/register/" method="post">
-        <CSRFtoken />
-
-        <input type="text" name="username" placeholder="Username" />
-        <input type="text" name="email" placeholder="Email" />
-        <input type="password" name="password" placeholder="Password" />
-        <input type="submit" />
-      </form>
+      <div className="LoginContainer">
+        <div className="LoginBlock" style={{ height: "720px" }}>
+          <img src={logo} width={"300px"} height={"300px"} alt="img" />
+          <div className="LoginForm">
+            <form action="/api/register/" method="post">
+              <CSRFtoken />
+              <TextField
+                name={"username"}
+                placeholder={"Username"}
+                autoFocus={"on"}
+              />
+              <br />
+              <TextField
+                name={"email"}
+                placeholder={"Email address"}
+                autoFocus={"on"}
+              />
+              <br />
+              <PasswordField name={"password"} placeholder={"Password"} />
+              <PasswordField
+                name={"password-confirm"}
+                placeholder={"Confirm Password"}
+              />
+              <SubmitField />
+              <HelperLabel
+                text={"If you already have an account "}
+                to={"/api/login/"}
+                toText={"Login"}
+              />
+            </form>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
